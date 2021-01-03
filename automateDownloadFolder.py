98a7215667ec2c41c9576c0b2aa10eaa0f1f95d7
Caroline Stoffel <carol_stoffel@hotmail.com>
2021-01-03 20:21:54 -0300

    refatorado o codigo, otimizado de 68 linhas para 37

--- automateDownloadFolder.py
+++ automateDownloadFolder.py
@@ -2,49 +2,36 @@ import os
 import shutil
 import getpass
 
-user = getpass.getuser() #gets automatically the system user name
+user = getpass.getuser()  # gets automatically the system user name
 directory = "C:\\Users\\{}\\Downloads".format(user)
-dir = os.chdir("{}".format(directory)) #changes the directory to what is inside the directory variable
+# changes the directory to what is inside the directory variable
+dir = os.chdir("{}".format(directory))
 
 EXTENSIONS = {}
-EXTENSIONS['EXT_AUDIO'] = ['.wav', '.mp3', '.raw', '.wma']
-EXTENSIONS['EXT_VIDEO'] = ['.mp4', '.m4a', 'm4v', 'f4v', 'f4a', 'm4b', 'm4r', 'f4b', 'mov']
-EXTENSIONS['EXT_DOCUMENTS'] = ['.txt', '.pdf', '.doc', '.docx', '.odt', '.html', '.csv', '.arff']
-EXTENSIONS['EXT_IMAGE'] = ['.jpeg', '.png', '.sgv', '.gif', '.bmp' ]
-EXTENSIONS['EXT_BOOKS'] = ['.epub']
-EXTENSIONS['EXT_PROGRAMAS'] = ['.exe']
-EXTENSIONS['EXT_COMPACT'] = ['.zip', '.rar']
-EXTENSIONS['EXT_SCRIPT'] = ['.py']
-DIRS = ['Audio', 'Videos', 'Documents', 'Images', 'Folders', 'Others', 'Books', 'Programas', 'Compact']
-
-for dir in DIRS:
-    fold = './'+dir
-    if not os.path.isdir(fold):
-        os.mkdir(fold)
-        print('Created the {} directory'.format(dir))
+EXTENSIONS['Audio'] = ['.wav', '.mp3', '.raw', '.wma']
+EXTENSIONS['Videos'] = ['.mp4', '.m4a', 'm4v',
+                        'f4v', 'f4a', 'm4b', 'm4r', 'f4b', 'mov']
+EXTENSIONS['Documents'] = ['.txt', '.pdf',
+                           '.doc', '.docx', '.odt', '.html', '.csv', '.arff', '.xls', '.xlsx']
+EXTENSIONS['Images'] = ['.jpeg', '.png', '.sgv', '.gif', '.bmp']
+EXTENSIONS['Books'] = ['.epub']
+EXTENSIONS['Programas'] = ['.exe']
+EXTENSIONS['Compact'] = ['.zip', '.rar']
 
 
 files = os.listdir()
-for file in files:
-    name, extension = os.path.splitext(file)
-    if extension in EXTENSIONS['EXT_AUDIO']:
-        shutil.move(file, './Audio/{}'.format(file))
-    elif extension in EXTENSIONS['EXT_DOCUMENTS']:
-        shutil.move(file, './Documents/{}'.format(file))
-    elif extension in EXTENSIONS['EXT_VIDEO']:
-        shutil.move(file, './Video/{}'.format(file))
-    elif extension in EXTENSIONS['EXT_IMAGE']:
-        shutil.move(file, './Images/{}'.format(file))
-    elif extension in EXTENSIONS['EXT_BOOKS']:
-        shutil.move(file, './Books/{}'.format(file))
-    elif extension in EXTENSIONS['EXT_COMPACT']:
-        shutil.move(file, './Compact/{}'.format(file))
-    elif os.path.isdir(file) and file not in DIRS:
-        shutil.move(file, './Folders/{}'.format(file))
-    elif extension == '.pem' or extension == '.ppk':
-        os.remove(file)
-        print('File {} removed!'.format(file))
-    elif extension in EXTENSIONS['EXT_PROGRAMAS']:
-        shutil.move(file, './Programas/{}'.format(file))
-    elif file not in DIRS and extension not in EXTENSIONS['EXT_SCRIPT']:
-        shutil.move(file, './Others/{}'.format(file))
+for i in EXTENSIONS.keys():
+    fold = './' + i
+    if not os.path.isdir(fold):
+        os.mkdir(fold)
+        print('Created the {} directory'.format(i))
+    for file in files:
+        name, extension = os.path.splitext(file)
+        if extension == '.py':
+            continue
+        if extension in EXTENSIONS[i]:
+            shutil.move(file, './{}/{}'.format(i, file))
+            print('File {} moved to {}'.format(file, i))
+        elif extension == '.pem' or extension == '.ppk':
+            os.remove(file)
+            print('File {} removed!'.format(file))
